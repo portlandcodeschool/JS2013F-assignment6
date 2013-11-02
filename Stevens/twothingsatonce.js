56d495624d0448dafc29f584bb13574e6fa5e53e
abbystevens <adeats@gmail.com>
2013-11-02 13:10:26 -0700

    initial commit for Stevens branch

---
+++ Stevens/twothingsatonce.js
@@ -0,0 +1,13 @@
+var read = require('read');
+var secondCounter = 0;
+
+read({ prompt: "What's going on?"}, function (err, result) {
+
+    console.log("You typed: " + result);
+    console.log(secondCounter);
+});
+
+  setInterval(function () {
+  	secondCounter++;
+  }, 1000);
+
