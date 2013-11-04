7a11625386aceffb1c38d29878e658a650708de1
garbagemanonfire <nilesvm@gmail.com>
2013-11-04 13:02:01 -0800

    tweaking to understand how two async process work.

--- mcgiver/twothingsatonce.js
+++ mcgiver/twothingsatonce.js
@@ -4,8 +4,13 @@ var secondCounter = 0;
 
 read({prompt: 'What\'s your name?'}, function (err, result) {
   console.log('Your name is: '+ result, 'it took you '+ secondCounter + ' seconds to respond.');
-  process.exit
+  read({prompt: "What's your favirote color"}, getColor);
 });
+
+function getColor (err, result, isDefault) {
+  console.log('Your color is: '+ result, 'it took you '+ secondCounter + ' seconds to respond.');
+  process.exit();
+}
 
 setInterval(function() {
   secondCounter++;
