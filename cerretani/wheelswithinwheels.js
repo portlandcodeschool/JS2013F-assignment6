39e3767d42f93929a1a06bc49c055839fa6d3172
Derek <derekcerretani@gmail.com>
2013-11-04 11:39:27 -0800

    Delete wheelswithinwheels.js

--- cerretani/wheelswithinwheels.js
+++
@@ -1,7 +0,0 @@
-/*Now we're going to combine the second and third assignments. 
-Write an asynchronous loop that runs a fixed number of times 
-within an outer loop that runs forever (until the user types 'exit'). 
-The inner loop should store its results with an array, as in assignment #3. 
-The outer loop will console.log() the latest contents of the array, 
-prompt the user and exit if the user types 'exit', and launch another 
-iteration of the fixed iteration inner loop otherwise.*/
